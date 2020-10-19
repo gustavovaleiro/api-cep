5acc801884b015332f6e6b6047c3ca2ac4444f7e
gustavo valeiro <zarpsystem_gvaleiro@orizon.com.br>
2020-10-19 10:16:10 -0300

    Mapeando entities

--- src/main/java/br/com/cep/model/Cep.java
+++ src/main/java/br/com/cep/model/Cep.java
@@ -1,16 +1,29 @@
 package br.com.cep.model;
 
-@SuppressWarnings("unused")
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TCep")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Cep {
-
+	@Id
+	@Column(name = "CEP",nullable = false, length = 10)
 	private String cep;
-
+	@Column(name = "LOGRADOURO", nullable = false, length = 255)
 	private String logradouro;
-
+	@Column(name = "COMPLEMENTO",  length = 255)
 	private String complemento;
-
+	@Column(name = "BAIRRO",  length = 50)
 	private String bairro;
 
+	@ManyToOne
+	@JoinColumn(name = "IBGE", nullable = false)
 	private Cidade cidade;
 
 }
--- src/main/java/br/com/cep/model/Cidade.java
+++ src/main/java/br/com/cep/model/Cidade.java
@@ -1,15 +1,27 @@
 package br.com.cep.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.List;
-@SuppressWarnings("unused")
+@Entity
+@Table(name = "TCidade")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Cidade {
-  
+
+  @Id
+  @Column(length = 20, nullable = false, unique = true)
   private String ibge;
-  
+
+  @Column(length = 2, nullable = false)
   private String uf;
-  
+  @Column(length = 100, nullable = false)
   private String localidade;
-  
+  @OneToOne(mappedBy = "cidade")
   private List<Cep> ceps; 
   
 }
